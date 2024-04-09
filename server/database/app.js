9e6fcdb48180a20780087d4cfe5f3edc7788260f
Alberto Camilo Rodriguez Vizcarra <proveedor152@culqi.com>
2024-04-09 06:54:21 -0500

    update jshint

--- server/database/app.js
+++ server/database/app.js
@@ -18,18 +18,19 @@ const Reviews = require('./review');
 
 const Dealerships = require('./dealership');
 
-try {
-  Reviews.deleteMany({}).then(()=>{
-    Reviews.insertMany(reviews_data['reviews']);
-  });
-  Dealerships.deleteMany({}).then(()=>{
-    Dealerships.insertMany(dealerships_data['dealerships']);
-  });
-  
-} catch (error) {
-  res.status(500).json({ error: 'Error fetching documents' });
-}
 
+// try {
+//   Reviews.deleteMany({}).then(()=>{
+//     Reviews.insertMany(reviews_data['reviews']);
+//   });
+//   Dealerships.deleteMany({}).then(()=>{
+//     Dealerships.insertMany(dealerships_data['dealerships']);
+//   });
+//
+// } catch (error) {
+//   res.status(500).json({ error: 'Error fetching documents' });
+// }
+let data
 
 // Express route to home
 app.get('/', async (req, res) => {
